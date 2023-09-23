9df9c4e1f82de6d137babd4f81a3d7cf0e2c7afc
Juanj020 <128321932+Juanj020@users.noreply.github.com>
2023-09-23 10:14:40 -0500

    feat: :tada: Configuracion de ṕagina
    Termine todo ese visaje bien bien pa aprender
    
    ala

---
+++ main.js
@@ -0,0 +1,10 @@
+const a = "sdfds";
+
+// U significa que lo acabo de crear
+// A ese archivo nuevo quiere que git empiece a tener control 
+// M Significa que hizo una modificación
+// D Cuando ute elimina
+// /* ! */ conflicto 
+// /* ? */ hay un conflicto entre ramas  
+
+console.log("hello world!");
